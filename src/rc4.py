0d693013af364d5067aa63cf05007147920551f6
claudiney63 <claudineyryan75@gmail.com>
2023-07-08 05:13:29 -0300

    news comments RC4

--- src/rc4.py
+++ src/rc4.py
@@ -1,6 +1,15 @@
 import os
 
 def generate_rc4_key(key_length=16):
+    """
+    Gera uma chave aleatória para o algoritmo RC4.
+
+    Args:
+        key_length (int): O tamanho da chave em bytes. O padrão é 16.
+
+    Returns:
+        bytes: A chave gerada.
+    """
     try:
         key = os.urandom(key_length)  # Gera uma sequência de bytes aleatórios como chave
         return key
@@ -8,25 +17,35 @@ def generate_rc4_key(key_length=16):
         raise Exception("Erro ao gerar a chave RC4:", e)
 
 def rc4(key, plaintext):
+    """
+    Criptografa ou descriptografa um texto usando o algoritmo RC4.
+
+    Args:
+        key (bytes): A chave para o algoritmo RC4.
+        plaintext (bytes): O texto a ser criptografado ou descriptografado.
+
+    Returns:
+        bytes: O texto criptografado ou descriptografado.
+    """
     try:
-        S = list(range(256))
+        S = list(range(256))  # Inicializa uma lista de 256 elementos com valores de 0 a 255
         j = 0
         out = []
 
         # Key-scheduling algorithm (KSA)
         for i in range(256):
-            j = (j + S[i] + key[i % len(key)]) % 256
-            S[i], S[j] = S[j], S[i]
+            j = (j + S[i] + key[i % len(key)]) % 256  # Gera um valor de j baseado na chave e nos valores atuais de j e S[i]
+            S[i], S[j] = S[j], S[i]  # Troca os valores de S[i] e S[j] para embaralhar a lista S
 
         # Pseudo-random generation algorithm (PRGA)
         i = j = 0
         for char in plaintext:
-            i = (i + 1) % 256
-            j = (j + S[i]) % 256
-            S[i], S[j] = S[j], S[i]
-            out.append(char ^ S[(S[i] + S[j]) % 256])
+            i = (i + 1) % 256  # Atualiza o valor de i
+            j = (j + S[i]) % 256  # Atualiza o valor de j
+            S[i], S[j] = S[j], S[i]  # Troca os valores de S[i] e S[j] para embaralhar a lista S novamente
+            out.append(char ^ S[(S[i] + S[j]) % 256])  # Realiza a operação XOR entre o caractere e um valor pseudoaleatório de S
 
-        return bytes(out)
+        return bytes(out)  # Retorna o texto criptografado ou descriptografado em formato de bytes
     except Exception as e:
         raise Exception("Erro ao executar o algoritmo RC4:", e)
 
