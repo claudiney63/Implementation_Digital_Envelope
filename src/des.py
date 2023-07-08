e21e1f17ae15c409cea6bf9192ea2fe558bc8b66
claudiney63 <claudineyryan75@gmail.com>
2023-07-08 04:34:43 -0300

    adição de try and except no DES

--- src/des.py
+++ src/des.py
@@ -4,63 +4,75 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 
 def encrypt(key, plaintext):
-    # Gerar uma chave de 8 bytes para o DES
-    key = key[:8]  # Se o tamanho da chave for maior que 8 bytes, pegamos apenas os primeiros 8 bytes
+    try:
+        # Gerar uma chave de 8 bytes para o DES
+        key = key[:8]  # Se o tamanho da chave for maior que 8 bytes, pegamos apenas os primeiros 8 bytes
 
-    # Gerar um vetor de inicialização (IV) de 8 bytes
-    iv = os.urandom(8)
+        # Gerar um vetor de inicialização (IV) de 8 bytes
+        iv = os.urandom(8)
 
-    # Criar um objeto Cipher usando a chave e o modo de operação CBC
-    cipher = Cipher(algorithms.TripleDES(key), modes.CBC(iv), backend=default_backend())
+        # Criar um objeto Cipher usando a chave e o modo de operação CBC
+        cipher = Cipher(algorithms.TripleDES(key), modes.CBC(iv), backend=default_backend())
 
-    # Criar um objeto de padding PKCS7
-    padder = padding.PKCS7(algorithms.TripleDES.block_size).padder()
+        # Criar um objeto de padding PKCS7
+        padder = padding.PKCS7(algorithms.TripleDES.block_size).padder()
 
-    # Realizar o padding do texto plano
-    padded_plaintext = padder.update(plaintext) + padder.finalize()
+        # Realizar o padding do texto plano
+        padded_plaintext = padder.update(plaintext) + padder.finalize()
 
-    # Criar um objeto de criptografia
-    encryptor = cipher.encryptor()
+        # Criar um objeto de criptografia
+        encryptor = cipher.encryptor()
 
-    # Criptografar o texto plano com o padding
-    ciphertext = encryptor.update(padded_plaintext) + encryptor.finalize()
+        # Criptografar o texto plano com o padding
+        ciphertext = encryptor.update(padded_plaintext) + encryptor.finalize()
 
-    return iv + ciphertext
+        return iv + ciphertext
+    except Exception as e:
+        print("Erro durante a criptografia:", e)
 
 def decrypt(key, ciphertext):
-    # Extrair o IV do início do texto cifrado
-    iv = ciphertext[:8]
-    ciphertext = ciphertext[8:]
+    try:
+        # Extrair o IV do início do texto cifrado
+        iv = ciphertext[:8]
+        ciphertext = ciphertext[8:]
 
-    # Criar um objeto Cipher usando a chave e o modo de operação CBC
-    key = key[:8]  # Se o tamanho da chave for maior que 8 bytes, pegamos apenas os primeiros 8 bytes
-    cipher = Cipher(algorithms.TripleDES(key), modes.CBC(iv), backend=default_backend())
+        # Criar um objeto Cipher usando a chave e o modo de operação CBC
+        key = key[:8]  # Se o tamanho da chave for maior que 8 bytes, pegamos apenas os primeiros 8 bytes
+        cipher = Cipher(algorithms.TripleDES(key), modes.CBC(iv), backend=default_backend())
 
-    # Criar um objeto de descriptografia
-    decryptor = cipher.decryptor()
+        # Criar um objeto de descriptografia
+        decryptor = cipher.decryptor()
 
-    # Descriptografar o texto cifrado
-    padded_plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+        # Descriptografar o texto cifrado
+        padded_plaintext = decryptor.update(ciphertext) + decryptor.finalize()
 
-    # Criar um objeto de unpadding PKCS7
-    unpadder = padding.PKCS7(algorithms.TripleDES.block_size).unpadder()
+        # Criar um objeto de unpadding PKCS7
+        unpadder = padding.PKCS7(algorithms.TripleDES.block_size).unpadder()
 
-    # Remover o padding do texto plano descriptografado
-    plaintext = unpadder.update(padded_plaintext) + unpadder.finalize()
+        # Remover o padding do texto plano descriptografado
+        plaintext = unpadder.update(padded_plaintext) + unpadder.finalize()
 
-    return plaintext
+        return plaintext
+    except Exception as e:
+        print("Erro durante a descriptografia:", e)
 
 # Exemplo de uso
 def generate_key(size):
-    return os.urandom(size)
+    try:
+        return os.urandom(size)
+    except Exception as e:
+        print("Erro durante a geração da chave:", e)
 
 if __name__ == '__main__':
-    key = generate_key(32)
-    plaintext = b'Ellem quer chocolate?'
+    try:
+        key = generate_key(24)  # Tamanho da chave é 24 bytes para o TripleDES
+        plaintext = b'Ellem quer chocolate?'
 
-    ciphertext = encrypt(key, plaintext)
-    decrypted_text = decrypt(key, ciphertext)
+        ciphertext = encrypt(key, plaintext)
+        decrypted_text = decrypt(key, ciphertext)
 
-    print("Chave DES:", key)
-    print("Texto cifrado:", ciphertext)
-    print("Texto descriptografado:", decrypted_text)
+        print("Chave DES:", key)
+        print("Texto cifrado:", ciphertext)
+        print("Texto descriptografado:", decrypted_text)
+    except Exception as e:
+        print("Erro no exemplo de uso:", e)
